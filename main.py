d29303be2d0f48ed59426e1c7162748abbb19271
Kavan <avkavan@gmail.com>
2024-05-15 11:00:11 +0530

    added proccess video function

--- main.py
+++ main.py
@@ -1,8 +1,56 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, Response, UploadFile, HTTPException
 from roboflow import Roboflow
 import os
+import subprocess
+from inference import InferencePipeline
+from roboflow import Roboflow
+from inference.core.interfaces.stream.sinks import VideoFileSink
 
 app = FastAPI()
+
+# Initialize the inference pipeline
+model_id = "merged-project-2/1"
+output_file_name = "outputfile.mp4"
+
+
+ 
+@app.post("/process_video")
+async def process_video(video_file: UploadFile = File(...)):
+    # Save the uploaded video to a local file
+    with open("uploaded_video.mp4", "wb") as buffer:
+        buffer.write(await video_file.read())
+
+    # Initialize VideoFileSink
+    video_sink = VideoFileSink.init(video_file_name="outputfile.avi")
+
+    # Initialize and start the inference pipeline
+    pipeline = InferencePipeline.init(
+        model_id=model_id,
+        api_key= "INc4g2WbMuzVOyCAXNVp",
+        video_reference="uploaded_video.mp4",
+        on_prediction=video_sink.on_prediction
+    )
+    pipeline.start()
+    pipeline.join()
+    video_sink.release()
+
+    # Convert outputfile.avi to outputfile.mp4
+    conversion_command = ["ffmpeg", "-y", "-i", "outputfile.avi", "outputfile.mp4"]
+    subprocess.run(conversion_command)
+
+    # Read the processed video file
+    with open("outputfile.mp4", "rb") as video_file:
+        video_data = video_file.read()
+
+    # Clean up local files
+    os.remove("outputfile.avi")
+    os.remove("outputfile.mp4")
+    os.remove("uploaded_video.mp4")
+
+    # Return the processed video and results as response
+    return Response(content=video_data, media_type="video/mp4")
+
+
 
 rf = Roboflow(api_key="INc4g2WbMuzVOyCAXNVp")
 project = rf.workspace().project("merged-project-2")
